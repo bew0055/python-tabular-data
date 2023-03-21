b03f37e0121f48105fbba14a58144ce73d936361
Britt <bew0055@auburn.edu>
2023-03-21 10:11:38 -0500

    First attempt

---
+++ three_samples_regression.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+
+"""
+The purpose of this script is to create linear regressions of three different species on a scatter 
+plot using data from the 'iris.csv' file. This can be done by following the tutorial from this 
+assignment and breaking it into three seperate sets of data.
+"""
+
+def plot_data():
+    dataframe = pd.read_csv("iris.csv")
+    
+    setosa = dataframe[dataframe.species == "Iris_setosa"]
+    virginica = dataframe[dataframe.species == "Iris_virginica"]
+    versicolor = dataframe[dataframe.species == "Iris_versicolor"]
+ 
+    a = setosa.petal_length_cm
+    b = setosa.sepal_length_cm
+    regression = stats.linregress(a, b)
+    slope = regression.slope
+    intercept = regression.intercept
+    plt.scatter(a, b, label = 'Setosa Petal v Sepal Lengths (cm)')
+    plt.plot(a, slope * a + intercept, color = "red", label = 'Fitted line')
+    plt.xlabel("Petal length (cm)")
+    plt.ylabel("Sepal length (cm)")
+    plt.legend()
+    plt.savefig("Setosa_regress.png")
+
+    c = virginica.petal_length_cm
+    d = virginica.sepal_length_cm
+    regression = stats.linregress(c, d)
+    slope = regression.slope
+    intercept = regression.intercept
+    plt.scatter(c, d, label = 'Virginica Petal v Sepal Lengths (cm)')
+    plt.plot(c, slope * c + intercept, color = "blue", label = 'Fitted line')
+    plt.xlabel("Petal length (cm)")
+    plt.ylabel("Sepal length (cm)")
+    plt.legend()
+    plt.savefig("Virginica_regress.png")
+
+    x = versicolor.petal_length_cm
+    y = versicolor.sepal_length_cm
+    regression = stats.linregress(x, y)
+    slope = regression.slope
+    intercept = regression.intercept
+    plt.scatter(x, y, label = 'Versicolor Petal v Sepal Lengths (cm)')
+    plt.plot(x, slope * x + intercept, color = "green", label = 'Fitted line')
+    plt.xlabel("Petal length (cm)")
+    plt.ylabel("Sepal length (cm)")
+    plt.legend()
+    plt.savefig("Versicolor_regress.png")
+
+
+if __name__ == '__main__':
+    plot_data()
